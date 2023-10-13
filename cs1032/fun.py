a832164c35fa98cc6ad81a3c5c693d3fdfd76112
Nicholas Ashmead <104199862+NicholasAshmead@users.noreply.github.com>
2023-10-13 16:37:53 +0100

    week3, fun fix

--- cs1032/fun.py
+++ cs1032/fun.py
@@ -2,22 +2,19 @@ import random as r
 from time import sleep
 
 code = [
-'    number1 = input("Enter a number> ")',
-'        number1 = float(number1)',
-'        print("You need to enter an integer or float")',
-'    number2 = input("Enter another number> ")',
-'        number2 = float(number2)',
-'        print("You need to enter an integer or float")',
-'while True:',
-'    number3 = input("Enter a third number> ")',
-'        number3 = float(number3)',
+'    number1 = float(input("Enter a number> "))',
+'        if number1 == int(number1): return int(number1)',
+'while numberStatus == True:',
+'    number3 = int(input("Enter a third number> ").lower())',
 '        print("You need to enter an integer or float")',
 'def greatestOfThree(num1, num2, num3):',
 '    if num1 > num2:',
 '        if num1 > num3: return num1',
-'        else: return num3',
+'        for i in range (len(num1)):',
+'            if num1[i] > num3[i]: return num1',
+'        else: return num3.max(num1, num3)',
 '        if num2 > num3: return num2',
-'        else: return num',
+'        else: return num.round(num3, 2)',
 ]
 
 while True:
